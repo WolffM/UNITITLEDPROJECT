0b8da1eb41ec391fc7d8ddac80e786fe9d92db2f
agcollins <sullen.umbra@gmail.com>
2023-01-08 14:18:49 -0600

    Player changes direction

--- src/player.js
+++ src/player.js
@@ -1,9 +1,15 @@
 import {directions,anims} from "./config.js"
 
+const offset = 8
+const up = offset + 0
+const left = offset + 6
+const down = offset + 4
+const right = offset + 2
+
 // GameObject Player
 export class Player extends Phaser.GameObjects.Image {
     constructor(scene, x, y) {
-        super(scene, x, y, 'player', 8);
+        super(scene, x, y, 'player', offset);
 
         this.startX = x;
         this.startY = y;
@@ -17,18 +23,22 @@ export class Player extends Phaser.GameObjects.Image {
 
     update() {
         if (this.keyW.isDown) {
+            this.frame = this.texture.get(up)
             console.log("W")
             this.y--
         }
         if (this.keyA.isDown) {
+            this.frame = this.texture.get(left)
             console.log("A")
             this.x--
         }
         if (this.keyS.isDown) {
+            this.frame = this.texture.get(down)
             console.log("S")
             this.y++
         }
         if (this.keyD.isDown) {
+            this.frame = this.texture.get(right)
             console.log("D")
             this.x++
         }
